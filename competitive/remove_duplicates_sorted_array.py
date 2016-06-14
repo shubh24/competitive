75922b5b285b6a720917a505b57f65184dbc654c
Srivastava <shubhankar.srivastava@hpe.com>
2016-06-14 14:20:37 +0530

    should work

--- competitive/remove_duplicates_sorted_array.py
+++ competitive/remove_duplicates_sorted_array.py
@@ -1,9 +1,9 @@
 class Solution:
 	def removeDuplicates(self, A):
-		for i in range(len(A)):
-			for j in range(len(A)-1, i, -1):
-				if A[j] == A[i]:
-					del A[j]
+		for i in range(len(A)-1):
+			while (i+1 < len(A) and A[i+1] == A[i]):
+				del A[i+1]
+		print A
 		return len(A)
 
-print Solution().removeDuplicates([0])
+print Solution().removeDuplicates([0,1,1,1])
